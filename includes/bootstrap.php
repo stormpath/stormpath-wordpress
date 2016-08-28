b32553e5322b36b12d8a4987d6453a3b741f3654
Brian Retterer <brian@stormpath.com>
2016-08-28 00:04:47 -0400

    update phpcs

---
+++ includes/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Stormpath WordPress is a WordPress plugin to authenticate against a Stormpath Directory.
+ * Copyright (C) 2016  Stormpath
+ *
+ * This file is part of Stormpath WordPress.
+ *
+ * Stormpath WordPress is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Stormpath WordPress is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Stormpath
+ */
+
+namespace Stormpath;
+
+/**
+ * Auto-loader for the Stormpath WordPress plugin.
+ *
+ * @param string $className The class name that has been called.
+ *
+ * @return void
+ */
+function autoload( $className ) {
+
+	$class = substr( $className, strlen( __NAMESPACE__ ) );
+
+	$file   = sprintf( '%s/%s.php', __DIR__, str_replace( '\\', '/', $class ) );
+
+	if ( file_exists( $file ) ) {
+		require_once $file;
+	}
+}
--- stormpath.php
+++ stormpath.php
@@ -9,9 +9,12 @@
  * Text Domain: stormpath
  * Domain Path: /languages
  *
- * @package Stormpath
+ * @package  Stormpath
  */
+
+namespace Stormpath;
 
 define( 'STORMPATH_INTEGRATION',    'stormpath-wordpress' );
 define( 'STORMPATH_VERSION',        '0.0.0-alpha' );
 
+require_once __DIR__ . '/includes/bootstrap.php';
