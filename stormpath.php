9bd2051346b5af18947efc109cc071a4cfde397d
Brian Retterer <brian@stormpath.com>
2016-08-27 23:04:00 -0400

    replace index.php with stormpath.php entrypoint to plugin

---
+++ stormpath.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Stormpath
+ * Plugin URI: https://stormpath.com
+ * Description: Use Stormpath for your authentication
+ * Version: 0.0.0-alpha
+ * Author: Stormpath
+ * Author URI: https://stormpath.com
+ * Text Domain: stormpath
+ * Domain Path: /languages
+ *
+ * @package Stormpath
+ */
+
+define( 'STORMPATH_INTEGRATION',    'stormpath-wordpress' );
+define( 'STORMPATH_VERSION',        '0.0.0-alpha' );
