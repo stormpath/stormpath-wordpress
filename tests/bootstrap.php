0b1bc61c50f937a899d6c1b2d99501218f4d0c02
Brian Retterer <brian@stormpath.com>
2016-08-27 23:13:33 -0400

    add phpunit config

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Bootstrap the test suite.
+ *
+ * @package Stormpath
+ */
+
+if ( ! file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
+    throw new PHPUnit_Framework_Exception(
+        'ERROR: You must use Composer to install the test suite\'s dependencies!' . PHP_EOL
+    );
+}
+require_once __DIR__ . '/../vendor/autoload.php';
+
+WP_Mock::setUsePatchwork( true );
+WP_Mock::bootstrap();
+WP_Mock::tearDown();
